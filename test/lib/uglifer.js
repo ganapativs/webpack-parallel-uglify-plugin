8c5c2878f8bbca14c176c768f8a52913266be2c4
Ganapati V S <ganapati.vs@tracxn.com>
2017-06-23 15:34:25 +0530

    Sync with branch 'master' of github.com:gdborton/webpack-parallel-uglify-plugin

--- test/lib/uglifer.js
+++ test/lib/uglifer.js
@@ -1,78 +1,94 @@
 import test from 'ava';
 import sinon from 'sinon';
-import fs from 'fs';
 import os from 'os';
-import childProcess from 'child_process';
-import path from 'path';
 
-const stubbedOn = sinon.stub(process, 'on');
 const {
-    createWorkers,
-    minify,
-    workerCount,
+  workerCount,
+  processAssets,
 } = require('../../lib/uglifier');
 
-stubbedOn.restore();
+const filename = 'somefile.js';
+const testedFilename = 'testedFilename.js';
 
-let stubbedRead;
-let stubbedWrite;
-let stubbedDelete;
-test.beforeEach(() => {
-    stubbedRead = sinon.stub(fs, 'readFileSync', (filePath) => {
-        const fileName = path.basename(filePath, '.js');
-        if(fileName === 'throw') {
-            throw new Error('error');
-        }
-        return 'filecontents';
-    });
-    stubbedDelete = sinon.stub(fs, 'unlinkSync');
-    stubbedWrite = sinon.stub(fs, 'writeFileSync');
-});
+const unminifedSource = 'function    name()   {   }';
+const minifiedSource = 'function name(){}';
 
-const fakeWorker = {
-    on: () => {
+function createFakeCompilationObject() {
+  return {
+    assets: {
+      [filename]: {
+        source() {
+          return unminifedSource;
+        },
+      },
+      [testedFilename]: {
+        source() {
+          return unminifedSource;
+        },
+      },
     },
-    send: () => {
-    },
-    setMaxListeners: () => {
-    },
-};
-
-test.afterEach(() => {
-    stubbedRead.restore();
-    stubbedWrite.restore();
-    stubbedDelete.restore();
-});
+    options: {},
+  };
+}
 
 test('workerCount should be cpus - 1 if assetCount is >= cpus', t => {
-    const cpuStub = sinon.stub(os, 'cpus', () => ({length: 8}));
-    const assetCount = 10;
-    t.is(workerCount(assetCount), 7);
-    cpuStub.restore();
+  const cpuStub = sinon.stub(os, 'cpus', () => ({ length: 8 }));
+  const assetCount = 10;
+  t.is(workerCount(assetCount), 7);
+  cpuStub.restore();
 });
 
 test('workerCount should be assetCount if assetCount is < cpus', t => {
-    const cpuStub = sinon.stub(os, 'cpus', () => ({length: 8}));
-    const assetCount = 5;
-    t.is(workerCount(assetCount), 5);
-    cpuStub.restore();
+  const cpuStub = sinon.stub(os, 'cpus', () => ({ length: 8 }));
+  const assetCount = 5;
+  t.is(workerCount(assetCount), 5);
+  cpuStub.restore();
 });
 
-test('createWorkers should fork x times', t => {
-    const stubbedFork = sinon.stub(childProcess, 'fork', () => fakeWorker);
-    createWorkers(12);
-    t.is(stubbedFork.callCount, 12);
-    stubbedFork.restore();
+test.cb('processAssets minifies each of the assets in the compilation object', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
+  processAssets(fakeCompilationObject, {}).then(() => {
+    const minifiedResult = fakeCompilationObject.assets[filename].source();
+    t.is(minifiedResult, minifiedSource);
+    t.end();
+  });
 });
 
-test('minify should return a Promise', t => {
-    const promise = minify(
-        'uglifier.js', // assetName
-        {source: () => 'asdf;'}, // asset
-        false, // useSourceMaps
-        fakeWorker, // worker
-        {}
-    );
-    // ava uses babel for tests so Promise probably isn't node Promise.
-    t.is(typeof promise.then, 'function');
+test.cb('processAssets respects test option', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
+  processAssets(fakeCompilationObject, {
+    test: /tested/,
+  }).then(() => {
+    const unmatchedResult = fakeCompilationObject.assets[filename].source();
+    const matchedResult = fakeCompilationObject.assets[testedFilename].source();
+    t.is(unmatchedResult, unminifedSource);
+    t.is(matchedResult, minifiedSource);
+    t.end();
+  });
+});
+
+test.cb('processAssets respects include option', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
+  processAssets(fakeCompilationObject, {
+    include: [/tested/],
+  }).then(() => {
+    const unmatchedResult = fakeCompilationObject.assets[filename].source();
+    const matchedResult = fakeCompilationObject.assets[testedFilename].source();
+    t.is(unmatchedResult, unminifedSource);
+    t.is(matchedResult, minifiedSource);
+    t.end();
+  });
+});
+
+test.cb('processAssets respects exclude option', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
+  processAssets(fakeCompilationObject, {
+    exclude: [/tested/],
+  }).then(() => {
+    const unmatchedResult = fakeCompilationObject.assets[filename].source();
+    const matchedResult = fakeCompilationObject.assets[testedFilename].source();
+    t.is(unmatchedResult, minifiedSource);
+    t.is(matchedResult, unminifedSource);
+    t.end();
+  });
 });
